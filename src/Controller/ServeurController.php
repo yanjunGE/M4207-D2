4d10afb3f6979a69bd4163a23ce21329886067f0
yanjun <geyanjun8@gmail.com>
2022-02-02 16:18:41 +0100

    correction

--- src/Controller/ServeurController.php
+++ src/Controller/ServeurController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ServeurController extends AbstractController
 {
@@ -17,7 +18,9 @@ class ServeurController extends AbstractController
             'controller_name' => 'ServeurController',
         ]);
         
-    }
+    } 
+      
+    
             
     
 }
