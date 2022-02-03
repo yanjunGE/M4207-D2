e99bb3bfdcbc1f591ad8d583488997f3b5ee7230
yanjun <geyanjun8@gmail.com>
2022-02-03 10:02:41 +0100

    utilisateur root

--- src/Controller/ServeurController.php
+++ src/Controller/ServeurController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,13 +13,45 @@ class ServeurController extends AbstractController
     /**
      * @Route("/serveur", name="serveur")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         return $this->render('serveur/index.html.twig', [
             'controller_name' => 'ServeurController',
         ]);
         
+       
+      
+    }
+        
+    /**
+     * @Route("/valide", name="valide")
+     */
+    public function valide(Request $request): Response
+    {
+        $login=$request->request->get("root");
+        $password=$request->request->get("valide");
+
+        
+            return $this->rendre('serveur/valide.html.twig', [
+                'controller_name' => 'ServeurController',
+                'login'=> $login,
+                'password'=> $password,
+            ]);
+        
+
+    }
+    /**
+     * @Route("/signup", name="creerutilisateur")
+     */
+    public function signup(): Response
+    {
+        return $this->render('serveur/creerutilisateur.html.twig', [
+            'controller_name' => 'ServeurController',
+        ]);
+
+        
     } 
+
       
     
             
--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -1,10 +1,9 @@
+
 <?php
 
 namespace App\Entity;
 
 use App\Repository\UtilisateurRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -28,16 +27,6 @@ class Utilisateur
      * @ORM\Column(type="string", length=255)
      */
     private $password;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Acces::class, mappedBy="util")
-     */
-    private $acces;
-
-    public function __construct()
-    {
-        $this->acces = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
@@ -64,36 +53,6 @@ class Utilisateur
     public function setPassword(string $password): self
     {
         $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Acces[]
-     */
-    public function getAcces(): Collection
-    {
-        return $this->acces;
-    }
-
-    public function addAcce(Acces $acce): self
-    {
-        if (!$this->acces->contains($acce)) {
-            $this->acces[] = $acce;
-            $acce->setUtil($this);
-        }
-
-        return $this;
-    }
-
-    public function removeAcce(Acces $acce): self
-    {
-        if ($this->acces->removeElement($acce)) {
-            // set the owning side to null (unless already changed)
-            if ($acce->getUtil() === $this) {
-                $acce->setUtil(null);
-            }
-        }
 
         return $this;
     }
