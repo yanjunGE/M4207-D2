9ffc5ddcabc5ff982093091940eb0936676bcf0a
yanjun <geyanjun8@gmail.com>
2022-03-06 20:06:14 +0100

    session

--- src/Controller/ServeurController.php
+++ src/Controller/ServeurController.php
@@ -11,11 +11,12 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Utilisateur;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class ServeurController extends AbstractController
 {
     /**
-     * @Route("/serveur", name="serveur")
+     * @Route("/login", name="login")
      */
     public function index(Request $request): Response
     {
@@ -26,53 +27,106 @@ class ServeurController extends AbstractController
        
       
     }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    
+    public function logout(Request $request,SessionInterface $session): Response
+    {
+		
+		$session = $request->getSession();
+		$session->clear();
+		
+        return $this->redirectToRoute('login');
+    }
+    
+   
+    
+   
         
     /**
      * @Route("/valide", name="valide")
      */
-    public function valide(Request $request,EntityManagerInterface $manager): Response
+    public function valide(Request $request,EntityManagerInterface $manager,SessionInterface $session): Response
     {
         $login=$request->request->get("login");
        $password=$request->request->get("password");
-       
+       dump($login,$password);
+		$val=0;
 
         $reponse = $manager -> getRepository(Utilisateur::class) -> findOneBy([ 'login' => $login]);
             if($reponse==NULL){
                 $valide="utilisateur n'exsit pas";
                
+               
 
             }else{
                 $motdepass=$reponse -> getPassword();
+                
                 if($motdepass==$password){
                     $valide="valide";
-                   
+                     $val=1;
+				    dump($val);
+                    $session = new Session();
+                    $userId=$reponse ->getId();
+                    $userId = $session -> set('userId',$userId);
+                  
+                    $session->set('idUser', $reponse->getId());
+                    dump($session->get('nameUser'));
+                   return $this->redirectToRoute('affiche');
                 }else{
                     $valide="password n'est pas correct";
                 }
             
             }
+            
             return $this->render('serveur/valide.html.twig', [
                     'controller_name' => 'ServeurController',
                     'login'=> $login,
                     'password'=> $password, 
-                    'valide'=> $valide
+                    'valide'=> $valide,
+                    
                     ]);
 
+    }
+
+    /**
+    * @Route("/supprimerUtilisateur/{id}",name="supprimer_Utilisateur")
+    */
+    public function supprimerUtilisateur(EntityManagerInterface $manager,Utilisateur $editutil): Response {
+        $manager->remove($editutil);
+        $manager->flush();
+        // Affiche de nouveau la liste des utilisateurs
+        return $this->redirectToRoute ('affiche');
     }
     /**
      * @Route("/signup", name="creerutilisateur")
      */
-    public function signup(): Response
+    public function signup(Request $request,EntityManagerInterface $manager,SessionInterface $session): Response
     { 
+        $session = $request->getSession();
+		
+		if($session->get('userId')==NULL){
+			
+			return $this->redirectToRoute('login');
+		}else{	
         return $this->render('serveur/creerutilisateur.html.twig', [
             'controller_name' => 'ServeurController',
         ]);
+    }
     } 
      /**
      * @Route("/newutilisateur", name="newutilisateur")
      */
     public function newutilisateur(Request $request,EntityManagerInterface $manager,SessionInterface $session): Response
     {
+        $session = $request->getSession();
+		
+		if($session->get('userId')==NULL){
+			
+			return $this->redirectToRoute('login');
+		}else{	
         $newlogin=$request->request->get("newlogin");
         $newpassword=$request->request->get("newpassword1");
         $newutilisateur= new Utilisateur();
@@ -81,18 +135,22 @@ class ServeurController extends AbstractController
         $manager -> persist($newutilisateur);
         $manager ->flush();
         return $this->redirectToRoute ('affiche');
+    }
           
     }
      /**
      * @Route("/affiche", name="affiche")
      */
-    public function affiche(Request $request,EntityManagerInterface $manager): Response
+    public function affiche(Request $request,EntityManagerInterface $manager,SessionInterface $session): Response
     {
-        
+        if($session->get('userId')==NULL){
+			
+			return $this->redirectToRoute('login');
+		}else{	
         $mesUtilisateurs = $manager->getRepository(Utilisateur::class)->findAll();
         return $this->render('serveur/affiche.html.twig',['lst_utilisateurs' => $mesUtilisateurs]);
             
-        
+        }
 
         
     } 
@@ -102,14 +160,38 @@ class ServeurController extends AbstractController
     public function session(Request $request,EntityManagerInterface $manager,SessionInterface $session): Response
     {
         $vs = $session -> get('login');
-        $val=44;
-        $session -> set('nomVar',$val);
-        $utilisateur->getId();
+        $userId = $session -> get('userId');
+       
         $utilisateur = $manager -> getRepository(Utilisateur::class)->findOneById($userId);
-        $utilisateur->getId();
-        return $this->redirectToRoute ('affiche');
+        if ($utilisateur==NULL){
+            return $this->redirectToRoute ('logout');
+        }else{
+            return $this->redirectToRoute ('affiche');
+        }
+
+        //$val = 44;
+        //$session -> set('situation',$val);
+
+        
           
     }
+     /**
+     * @Route("/menu", name="menu")
+     */
+    public function menu(Request $request,EntityManagerInterface $manager,SessionInterface $session): Response
+    {
+        
+            return $this->redirectToRoute ('login');
+        
+
+        //$val = 44;
+        //$session -> set('situation',$val);
+
+        
+          
+    }
+    
+    
 
       
     
